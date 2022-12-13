e4accbb63c6768e006786080e28fa56c35ae1d31
PTiagorio <tiagorioferreira@hotmail.com>
2022-12-13 12:07:30 +0000

    Small improvement of a comment.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -106,7 +106,7 @@ contract LotteryContract is Owner, VRFV2WrapperConsumerBase, ConfirmedOwner {
     function fulfillRandomWords(uint256 /*requestId*/, uint256[] memory randomness) internal virtual override  {
         randomResult = randomness[0] % (Lottery.ticketsAmount);
         uint amountToTransfer = Lottery.potAmount - (Lottery.potAmount * 2 / 100);
-        // TODO: transfer can revert, and "fulfillRandomWords" should never revert, accordingly to the chainlink documentation
+        // TODO: "transfer" function can revert, and "fulfillRandomWords" should never revert, accordingly to the chainlink documentation
         // (source: https://docs.chain.link/vrf/v2/security/#fulfillrandomwords-must-not-revert)
         // to fix this, we either should make a synchronous call of this code or run an Automation Node that make this call in another function
         // for now, it's like this due to convenience when using the contract and to not use a chainlink subscription (for the Automation Node)
